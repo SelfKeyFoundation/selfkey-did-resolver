31e1fd74d9bdd2b19c61187772ebba9565ce6164
Carlos Bruguera <cbruguera@gmail.com>
2018-05-29 18:52:41 +0700

    Fixed errors and integrated connection with NameRegistry contract

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const
+	util = require('util')
 	fs = require('fs')
 	path = require('path')
 	Web3 = require('web3')
@@ -9,20 +10,25 @@ const
 		3: "ERC725ClaimSignerKey",
 		4: "ERC725EncryptionKey"
 	}
+	nameRegistryAddress = '0xffe08afb7fe148c42deba7ff80d6b9444ce95b6a'
 
 // retrieve the contract object from the network
-async function connector(address) {
+async function identityContractConnector(address) {
 	return new web3.eth.Contract(JSON.parse(fs.readFileSync(path.join(__dirname, './id-abi.json'))).abi, address)
+}
+
+async function nameRegistryConnector(address) {
+	return new web3.eth.Contract(JSON.parse(fs.readFileSync(path.join(__dirname, './NameRegistry.json'))).abi, address)
 }
 
 // handle multiple keys
 async function loopKeys(contract, did) {
-	const keysCount = await contract.methods.keysCount.call()
+	const keysCount = await contract.methods.keysCount().call()
 	let keys = []
 	for (let i = keysCount.length - 1; i >= 0; i--) {
 		const
-			hash = await contract.methods.keyHashes.call(i)
-			gotKey = await contract.methods.getKeyByHash(hash)
+			hash = await contract.methods.keyHashes(i).call()
+			gotKey = await contract.methods.getKeyByHash(hash).call()
 			key = {
 				id: `${did}#keys-` + (i + 1),
 				type: ['KoblitzVerificationKey2017', keyTypes[gotKey[1]]],
@@ -36,14 +42,14 @@ async function loopKeys(contract, did) {
 
 // handle multiple service enpdpoints
 async function loopServices(contract) {
-	const servicesCount = await contract.methods.servicesCount.call()
+	const servicesCount = await contract.methods.servicesCount().call()
 	let services = []
 	for (let i = servicesCount.length - 1; i >= 0; i--) {
-		const 
-			serviceType = await contract.methods.services.call(i)
-			endpoint = await contract.methods.getServiceByType(serviceType)
+		const
+			serviceType = await contract.methods.services(i).call()
+			endpoint = await contract.methods.getServiceByType(serviceType).call()
 			service = {
-				type: web3.toAscii(serviceType).replace(/\0/g, ''),
+				type: web3.utils.toAscii(serviceType).replace(/\0/g, ''),
 				serviceEndpoint: endpoint
 			}
 		services.push(service)
@@ -53,34 +59,52 @@ async function loopServices(contract) {
 
 // build the DID Document
 async function buildDocument(did, address) {
-	const 
-		contract = await connector(address)
-		owner = await contract.methods.owner.call()
-		keys = await loopKeys(contract, did)
-		services = await loopServices(contract)
 	let doc = {
 		'@context': 'https://w3id.org/did/v1',
 		id: did
 	}
-	if (keys.length > 0) doc.publicKey = keys
-	if (services.length > 0) doc.service = services
-	if (owner == '0x') {
+
+	if (await web3.eth.getCode(address) != "0x") {
+		const
+			contract = await identityContractConnector(address)
+			owner = await contract.methods.owner().call()
+			keys = await loopKeys(contract, did)
+			services = await loopServices(contract)
+
+		if (keys.length > 0) doc.publicKey = keys
+		if (services.length > 0) doc.service = services
+	} else {
 		doc.publicKey = [
 			{
 				id: `${did}#keys-1`,
 				type: ['KoblitzVerificationKey2017'],
 				owner: did,
 				publicAddress: address
-			}
-		]
+		}]
 	}
+
 	return doc
 }
 
 // resolve the DID and return the DID document
 async function didResolver(did) {
-	if (!did.match(/^did:key:0x[0-9a-fA-F]{40}$/)) return { error: 'Not a valid SelfKey DID' }
-	const address = did.match(/0x[0-9a-fA-F]{40}/)[0]
+	let address, name
+
+	if (!did.match(/^did:key:0x[0-9a-fA-F]{40}$/)) {
+		if (!did.match(/^did:key:\w{0,32}$/)) {
+			return { error: 'Not a valid SelfKey DID' }
+		} else {
+			const nameRegistry = await nameRegistryConnector(nameRegistryAddress)
+			name = did.match(/\w{0,32}$/)[0]
+			address = await nameRegistry.methods.resolveName(web3.utils.asciiToHex(name)).call()
+			
+			if (Number(address) == 0) {
+				return { error: 'Not a valid SelfKey DID' }
+			}
+		}
+	} else {
+		address = did.match(/0x[0-9a-fA-F]{40}/)[0]
+	}
 	return buildDocument(did, address)
 }
 
