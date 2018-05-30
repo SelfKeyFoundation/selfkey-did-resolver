ce204c5787d908166aad507e711b6916b5d2a59e
altninja <ben@grvs.io>
2018-05-30 07:07:22 +0700

    add use strict and update const declarations

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const
-	util = require('util')
-	fs = require('fs')
-	path = require('path')
-	Web3 = require('web3')
-	web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'))
-	keyTypes = {
+'use strict'
+
+const util = require('util')
+const fs = require('fs')
+const path = require('path')
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'))
+const keyTypes = {
 		1: "ERC725ManagementKey",
 		2: "ERC725ActionKey",
 		3: "ERC725ClaimSignerKey",
 		4: "ERC725EncryptionKey"
 	}
-	nameRegistryAddress = '0xffe08afb7fe148c42deba7ff80d6b9444ce95b6a'
+const nameRegistryAddress = '0xffe08afb7fe148c42deba7ff80d6b9444ce95b6a'
 
 // retrieve the contract object from the network
 async function identityContractConnector(address) {
@@ -26,10 +27,9 @@ async function loopKeys(contract, did) {
 	const keysCount = await contract.methods.keysCount().call()
 	let keys = []
 	for (let i = keysCount.length - 1; i >= 0; i--) {
-		const
-			hash = await contract.methods.keyHashes(i).call()
-			gotKey = await contract.methods.getKeyByHash(hash).call()
-			key = {
+		const hash = await contract.methods.keyHashes(i).call()
+		const gotKey = await contract.methods.getKeyByHash(hash).call()
+		const key = {
 				id: `${did}#keys-` + (i + 1),
 				type: ['KoblitzVerificationKey2017', keyTypes[gotKey[1]]],
 				owner: did,
@@ -45,10 +45,9 @@ async function loopServices(contract) {
 	const servicesCount = await contract.methods.servicesCount().call()
 	let services = []
 	for (let i = servicesCount.length - 1; i >= 0; i--) {
-		const
-			serviceType = await contract.methods.services(i).call()
-			endpoint = await contract.methods.getServiceByType(serviceType).call()
-			service = {
+		const serviceType = await contract.methods.services(i).call()
+		const endpoint = await contract.methods.getServiceByType(serviceType).call()
+		const service = {
 				type: web3.utils.toAscii(serviceType).replace(/\0/g, ''),
 				serviceEndpoint: endpoint
 			}
@@ -63,14 +62,11 @@ async function buildDocument(did, address) {
 		'@context': 'https://w3id.org/did/v1',
 		id: did
 	}
-
 	if (await web3.eth.getCode(address) != "0x") {
-		const
-			contract = await identityContractConnector(address)
-			owner = await contract.methods.owner().call()
-			keys = await loopKeys(contract, did)
-			services = await loopServices(contract)
-
+		const contract = await identityContractConnector(address)
+		const owner = await contract.methods.owner().call()
+		const keys = await loopKeys(contract, did)
+		const services = await loopServices(contract)
 		if (keys.length > 0) doc.publicKey = keys
 		if (services.length > 0) doc.service = services
 	} else {
@@ -82,14 +78,12 @@ async function buildDocument(did, address) {
 				publicAddress: address
 		}]
 	}
-
 	return doc
 }
 
 // resolve the DID and return the DID document
 async function didResolver(did) {
 	let address, name
-
 	if (!did.match(/^did:key:0x[0-9a-fA-F]{40}$/)) {
 		if (!did.match(/^did:key:\w{0,32}$/)) {
 			return { error: 'Not a valid SelfKey DID' }
@@ -97,7 +91,6 @@ async function didResolver(did) {
 			const nameRegistry = await nameRegistryConnector(nameRegistryAddress)
 			name = did.match(/\w{0,32}$/)[0]
 			address = await nameRegistry.methods.resolveName(web3.utils.asciiToHex(name)).call()
-			
 			if (Number(address) == 0) {
 				return { error: 'Not a valid SelfKey DID' }
 			}
