a33d1a95407c1a18a4a9896647d5c9adab996086
altninja <ben@grvs.io>
2018-05-30 08:11:25 +0700

    fix test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
-const 
-	selfkeyResolver = require('selfkey-did-resolver')
-	did = 'did:key:0x62dfbf9cee9c6eded64d7e0b1e72165f0d26a7c8'
+'use strict'
+
+const selfkeyResolver = require('../src/index.js')
+const did = 'did:key:0x62dfbf9cee9c6eded64d7e0b1e72165f0d26a7c8'
 
 async function runTest(did) {
 	console.log('Begin DID Resolver Test...')
